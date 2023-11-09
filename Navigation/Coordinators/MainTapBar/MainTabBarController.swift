c5829183e72f90b26369232deaeebff32c998dee
Skorodumov.D.S <eparis@atlassian.com>
2023-11-09 22:02:38 +0300

    HW_4_8

--- Navigation/Coordinators/MainTapBar/MainTabBarController.swift
+++ Navigation/Coordinators/MainTapBar/MainTabBarController.swift
@@ -19,11 +19,12 @@ final class MainTabBarController : UITabBarController {
         let feedNavController = UINavigationController()
         let profileNavController = UINavigationController()
         let likedNavController = UINavigationController()
+        let myGeolocationController = UINavigationController()
         
         let feedVC = Factory(flow: .feed, login: "", navigation: feedNavController)
         let profileVC = Factory(flow: .profileinfo, login: "", navigation: profileNavController)
         let likedVC = Factory(flow: .liked, login: "", navigation: likedNavController)
-        
-        viewControllers = [feedVC.navigationController, profileVC.navigationController, likedVC.navigationController]
+        let geoVC = Factory(flow: .geo, login: "", navigation: myGeolocationController)
+        viewControllers = [feedVC.navigationController, profileVC.navigationController, likedVC.navigationController, geoVC.navigationController]
     }
 }
